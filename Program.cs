69f7c68b8a9c85cf11434adcb9c3572be4041db6
Donald189 <donaldkarey@gmail.com>
2022-03-06 21:58:36 +0530

    Peak and Pop from stack

--- Program.cs
+++ Program.cs
@@ -5,20 +5,52 @@ namespace StacksAndQueues
     class Program
     {
         /// <summary>
-        /// UC1 Stack and Queues push 70,30,56 values.
+        /// UC2 Stack 70,30,56 values are Peek, Pop and empty the stack.
         /// </summary>
+        /// <param name="args">The arguments.</param>
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Stack<int> st = new Stack<int>();
             st.Push(70);
             st.Push(30);
             st.Push(56);
 
+            Console.WriteLine("Before Pop the elements is");
+
             foreach (int i in st)
             {
                 Console.WriteLine(i);
             }
+            Console.WriteLine("the pick element is=" + st.Peek());
+
+            Console.WriteLine("After 1st time pop elements is");
+
+            st.Pop();
+
+            foreach (int i in st)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("the pick element is=" + st.Peek());
+
+            Console.WriteLine("After 2st time pop elements");
+            st.Pop();
+
+            foreach (int i in st)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("the pick element is=" + st.Peek());
+
+            Console.WriteLine("After 3st time pop elements=0");
+            st.Pop();
+
+            foreach (int i in st)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Now stack is empty and Peek values also 0");
         }
     }
 }
